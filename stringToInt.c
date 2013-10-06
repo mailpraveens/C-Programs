9954bcb3965fed33324614a789417c347a8a9d07
Praveen S <mailpraveens@gmail.com>
2013-10-07 01:37:23 +0530

    Added more comments.

--- stringToInt.c
+++ stringToInt.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 // Program to convert a string to integer and testing by multiplying it
+// Scope for improvemnt, Doesn't consider the sign for integers
 int getIntFromString(char *a){
 	int result = 0;
+	// Check for the sign at the first position and add a flag which can be used later.
 	while(*a!='\0'){
 		int digit;
 		digit = a[0]-'0';
 		result = result * 10 + digit;
 		a++;
+
 	}
 	return result;
 }
